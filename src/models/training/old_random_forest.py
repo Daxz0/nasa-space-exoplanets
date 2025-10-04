b32fc0ed9de64153bd8346e9a9d4f2300cdf6109
Daxz <59543649+Daxz0@users.noreply.github.com>
2025-10-04 16:45:32 -0700

    up

--- src/models/training/old_random_forest.py
+++ src/models/training/old_random_forest.py
@@ -9,19 +9,19 @@ import matplotlib.pyplot as plt
 dataset_path = r"C:\Users\jchen\Downloads\nasa-space-exoplanets\src\data\keplar.csv"
 df = pd.read_csv(dataset_path, comment='#')
 
-X = df[['koi_depth', 'koi_duration', 'koi_period', 'koi_prad', 'koi_impact', 'koi_model_snr', 'koi_teq', 'koi_dor', 'koi_incl']]
+X = df[['koi_prad','koi_dicco_msky','koi_dikco_msky','koi_dor','koi_prad_err2','koi_period','koi_duration','koi_depth']]
 
-exclude = ['koi_disposition', 'koi_vet_stat', 'koi_vet_date', 'koi_pdisposition', 'koi_disp_prov', 'koi_comment', 'koi_eccen', 'koi_eccen_err1', 'koi_eccen_err2', 'koi_longp', 'koi_longp_err1', 'koi_longp_err2', 'koi_ingress', 'koi_ingress_err1', 'koi_ingress_err2', 'koi_fittype', 'koi_sma_err1', 'koi_sma_err2', 'koi_incl_err1', 'koi_incl_err2', 'koi_teq_err1', 'koi_teq_err2', 'koi_limbdark_mod', 'koi_ldm_coeff4', 'koi_ldm_coeff3', 'koi_parm_prov', 'koi_tce_delivname', 'koi_trans_mod', 'koi_model_dof', 'koi_model_chisq', 'koi_datalink_dvr', 'koi_datalink_dvs', 'koi_sage', 'koi_sage_err1', 'koi_sage_err2', 'koi_sparprov']
 
-# X = df[[col for col in df.columns if col.startswith('koi')]]
+# exclude = ['koi_disposition', 'koi_pdisposition', 'koi_model_snr', 'koi_fpflag_nt', 'koi_fpflag_ss', 'koi_fpflag_co', 'koi_fpflag_ec', 'koi_score']
+# X = df[[col for col in df.columns if col.startswith('koi') and col not in exclude]]
+
 y = df['koi_pdisposition']
-
 X = X.apply(pd.to_numeric, errors='coerce')
 X = X.fillna(X.mean())
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=5)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-rf_model = RandomForestClassifier(n_estimators=100, random_state=48, max_depth=5, min_samples_split=15, min_samples_leaf=1, max_features='sqrt')
+rf_model = RandomForestClassifier(n_estimators=100, random_state=48, max_depth=25, min_samples_split=15, min_samples_leaf=1, max_features='sqrt')
 rf_model.fit(X_train, y_train)
 
 y_pred = rf_model.predict(X_test)
